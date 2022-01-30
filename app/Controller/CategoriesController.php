ef1392154219bc535fbc4a32286156a4acac5d7a
Douglas A <douglassan1@hotmail.com>
2022-01-30 01:20:44 -0300

    Commit: tentando entender como funcionam variáveis para passar dados do banco para a tela

--- app/Controller/CategoriesController.php
+++ app/Controller/CategoriesController.php
@@ -4,4 +4,9 @@ App::uses('AppController', 'Controller');
 
 class CategoriesController extends AppController {
     public $scaffold;
+    public function list2()
+    {
+        $this->set('list2', $this->Category->find('all'));
+    }
+
 }
---
+++ app/View/Categories/scaffold.index.ctp
@@ -0,0 +1,2 @@
+<h1>Index do scaffold categorias</h1>
+<?php echo $list2[0][0]; ?>
